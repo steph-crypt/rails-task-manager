7a539566e78ec05432d4ca11cc79cb0f04cd6168
Sofia Stephenson <sgandsteph@gmail.com>
2019-11-12 16:58:31 +0100

    Push challenge

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,35 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params[:id])
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:id])
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
 end
